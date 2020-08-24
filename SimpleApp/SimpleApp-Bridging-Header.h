03da8e81796757e4e6781e6beb94fe57812695af
Jacob Hughes <jacob.hughes@myunidays.com>
2020-08-24 09:13:23 +0100

    Initial commit

---
+++ SimpleApp/SimpleApp-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  SimpleApp-BridgingHeader.h
+//  SimpleApp
+//
+//  Created by Jacob Hughes on 24/08/2020.
+//  Copyright © 2020 MyUNiDAYS. All rights reserved.
+//
+
+#import "PureLayout.h"
---
+++ SimpleApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SimpleApp
+//
+//  Created by Jacob Hughes on 24/08/2020.
+//  Copyright © 2020 MyUNiDAYS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
